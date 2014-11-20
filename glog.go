362616ec5c7b6c223b740a2323ecdd6518eb8a33
Weiran YUAN <yuanwr@gmail.com>
2014-11-20 18:18:33 +0800

    flag

--- glog.go
+++ glog.go
@@ -397,12 +397,12 @@ type flushSyncWriter interface {
 }
 
 func init() {
-	flag.BoolVar(&logging.toStderr, "logtostderr", false, "log to standard error instead of files")
-	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr", false, "log to standard error as well as files")
-	flag.Var(&logging.verbosity, "v", "log level for V logs")
-	flag.Var(&logging.stderrThreshold, "stderrthreshold", "logs at or above this threshold go to stderr")
-	flag.Var(&logging.vmodule, "vmodule", "comma-separated list of pattern=N settings for file-filtered logging")
-	flag.Var(&logging.traceLocation, "log_backtrace_at", "when logging hits line file:N, emit a stack trace")
+	flag.BoolVar(&logging.toStderr, "logtostderr_deepglint", false, "log to standard error instead of files")
+	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr_deepglint", false, "log to standard error as well as files")
+	flag.Var(&logging.verbosity, "v_deepglint", "log level for V logs")
+	flag.Var(&logging.stderrThreshold, "stderrthreshold_deepglint", "logs at or above this threshold go to stderr")
+	flag.Var(&logging.vmodule, "vmodule_deepglint", "comma-separated list of pattern=N settings for file-filtered logging")
+	flag.Var(&logging.traceLocation, "log_backtrace_at_deepglint", "when logging hits line file:N, emit a stack trace")
 
 	// Default stderrThreshold is ERROR.
 	logging.stderrThreshold = errorLog
